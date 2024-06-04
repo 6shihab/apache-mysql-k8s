e38161030fca6095f84860d963479b1a22d3e926
Shihabul Hasan Shihab <6shihab@gmail.com>
2024-06-04 13:12:50 +0600

    Add files via upload

---
+++ common.sh
@@ -0,0 +1,30 @@
+# using 'sudo su' is not a good practice.
+sudo swapoff -a
+sudo apt update
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+
+sudo systemctl enable docker 
+sudo usermod -aG docker ${USER}
+
+
+# install kubeadm
+
+
+sudo mkdir /etc/apt/keyrings/
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+
+sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
+
